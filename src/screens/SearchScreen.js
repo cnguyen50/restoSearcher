ef011887dcb5b1908c0fc5db901cfbaf74b32ac4
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-13 16:29:55 -0700

    made axios request and limit the search query to 50

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar'
+import yelp from '../api/yelp';
 
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([])
 
-  
-  
+  const searchAPI = async () => {
+    const res = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: 'vancouver'
+      }
+    })
+    setResults(res.data.businesses)
+  };
+
   return (
     <View>
       <SearchBar 
         term={term} 
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term has been submitted")}
+        onTermChange={setTerm}
+        onTermSubmit={searchAPI}
       />
       <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   )
 };
