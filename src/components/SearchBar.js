bf6f9f9fe849ecdd7278f78b1d258d12626695ce
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-13 15:37:31 -0700

    boiler plate for search bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Text>Search Bar</Text>
+    </View>
+  )
+};
+
+export default SearchBar;
