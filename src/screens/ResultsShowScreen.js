e159d52b95be0b9d739bb48928a7516a857c53cc
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-14 16:00:25 -0700

    boiler plate for ResultShowScreen

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native'
+
+const ResultsShowScreen = () => {
+  return (
+    <View>
+      <Text> Results Show</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ResultsShowScreen;
