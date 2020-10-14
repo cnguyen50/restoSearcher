e6ba2e2ac7058e3d0f1fd226ee3f77c4ecfa69b7
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-14 15:19:59 -0700

    created new component for result details

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+
+const ResultsDetail = ({ result }) => {
+  return (
+    <View>
+      <Text>{result.name}</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+
+  
+})
+
+export default ResultsDetail;
