d63eab8d0c7f625318c63c6f022cf97198272ce0
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-14 16:09:24 -0700

    revised with withNavigation component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,7 +5,7 @@ import useResults from '../hooks/useResults'
 import ResultsList from '../components/ResultsList'
 
 
-const SearchScreen = ({ navigation }) => {
+const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchAPI, results, errMsg ] = useResults();
 
@@ -27,17 +27,14 @@ const SearchScreen = ({ navigation }) => {
         <ResultsList 
           results={filterResultsByPrice('$')} 
           title="Cost Effective"
-          navigation={navigation}
         />
         <ResultsList 
           results={filterResultsByPrice('$$')} 
           title="Bit Pricier"
-          navigation={navigation}
         />
         <ResultsList 
           results={filterResultsByPrice('$$$')} 
           title="Big Spender"
-          navigation={navigation}
         />
       </ScrollView>
     </>
