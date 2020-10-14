140a725e716695d30edd61b8a0790b26d518705e
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-14 16:19:08 -0700

    async helper function to grab id

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import { View, StyleSheet, Text } from 'react-native'
+import yelp from '../api/yelp'
 
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null)
   const id = navigation.getParam('id')
-  console.log(id)
+  
+  const getResult =  async (id) => {
+    const res = await yelp.get(`/${id}`)
+    setResult(res.data)
+  };
+
+  useEffect (() => {
+    getResult(id)
+  }, []);
+
   return (
     <View>
-      <Text> Results Show</Text>
+      <Text>Results Show</Text>
     </View>
   )
 };
